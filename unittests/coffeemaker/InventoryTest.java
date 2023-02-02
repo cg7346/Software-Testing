aad156db111f4e36046e7ef57e27e11abc123585
AcrylicRobin <acrylicpuzzles@gmail.com>
2023-02-02 14:24:59 -0500

    added more unit tests to inventory and added some to recipe

--- unittests/coffeemaker/InventoryTest.java
+++ unittests/coffeemaker/InventoryTest.java
@@ -11,25 +11,28 @@ import coffeemaker.exceptions.*;
 
 public class InventoryTest extends TestCase{
     private Inventory inv;
-    private Recipe r;
 
     @Before
 	public void setUp() throws Exception {
         inv = new Inventory();
-        r = new Recipe();
         super.setUp();
 	}
 
 	@After
 	public void tearDown() throws Exception {
         inv = null;
-        r = null;
         super.setUp();
 	}
 
     @Test
     public void testgetChocolate() {
         assertEquals(inv.getChocolate(), 15);
+    }
+
+    @Test
+    public void testsetChocolate() {
+        inv.setChocolate(10);
+        assertEquals(inv.getChocolate(), 10);
     }
 
     @Test
@@ -54,24 +57,56 @@ public class InventoryTest extends TestCase{
     }
 
     @Test
-    public void testenoughIngredientsYes() {
+    public void testaddChocolateInvalidNeg() {
         try {
-            r.setAmtChocolate("15");
-        } catch (RecipeException ex) {
-            fail("Should parse int");
+            inv.addChocolate("-10");
+        } catch (InventoryException ex) {
+            assertEquals(ex.getMessage(), "Units of chocolate must be a positive integer");
+            return;
         };
-        assertTrue(inv.enoughIngredients(r));
+        fail("Should not parse invalid input");
     }
 
     @Test
-    public void testenoughIngredientsChocolateNo() {
-        try {
-            r.setAmtChocolate("16");
-        } catch (RecipeException ex) {
-            fail("Should parse int");
-        };
-        assertFalse(inv.enoughIngredients(r));
+    public void testGetCoffee() {
+        assertEquals(inv.getCoffee(), 15);
     }
 
+    @Test
+    public void testsetCoffee() {
+        inv.setCoffee(10);
+        assertEquals(inv.getCoffee(), 10);
+    }
 
+    @Test
+    public void testaddCoffee0() {
+        try {
+            inv.addCoffee("0");
+        } catch (InventoryException ex) {
+            fail("Should parse int");
+        };
+        assertEquals(inv.getCoffee(), 15);
+    }
+
+    @Test
+    public void testaddCoffeeInvalid() {
+        try {
+            inv.addCoffee("abc");
+        } catch (InventoryException ex) {
+            assertEquals(ex.getMessage(), "Units of coffee must be a positive integer");
+            return;
+        };
+        fail("Should not parse invalid input");
+    }
+
+    @Test
+    public void testaddCoffeeInvalidNeg() {
+        try {
+            inv.addCoffee("-10");
+        } catch (InventoryException ex) {
+            assertEquals(ex.getMessage(), "Units of coffee must be a positive integer");
+            return;
+        };
+        fail("Should not parse invalid input");
+    }
 }
--- unittests/coffeemaker/RecipeTest.java
+++ unittests/coffeemaker/RecipeTest.java
@@ -1,5 +1,48 @@
 package coffeemaker;
 
-public class RecipeTest {
-    
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import coffeemaker.exceptions.RecipeException;
+import junit.framework.TestCase;
+
+public class RecipeTest extends TestCase {
+    private Inventory inv;
+    private Recipe r;
+
+    @Before
+	public void setUp() throws Exception {
+        inv = new Inventory();
+        r = new Recipe();
+        super.setUp();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+        inv = null;
+        r = null;
+        super.setUp();
+	}
+
+    @Test
+    public void testenoughIngredientsYes() {
+        try {
+            r.setAmtChocolate("15");
+        } catch (RecipeException ex) {
+            fail("Should parse int");
+        };
+        assertTrue(inv.enoughIngredients(r));
+    }
+
+    @Test
+    public void testenoughIngredientsChocolateNo() {
+        try {
+            r.setAmtChocolate("16");
+        } catch (RecipeException ex) {
+            fail("Should parse int");
+        };
+        assertFalse(inv.enoughIngredients(r));
+    }
+
 }
