56932c57b3ff2d507216c9ec36e54be5c6514516
Celeste Gambardella <cg7346@rit.edu>
2023-02-03 13:28:11 -0500

    added some more tests

--- unittests/coffeemaker/RecipeBookTest.java
+++ unittests/coffeemaker/RecipeBookTest.java
@@ -30,12 +30,15 @@ public class RecipeBookTest extends TestCase {
     }
 
     @Test
-    public void testGetRecipes() throws RecipeException {
-        r.setName("milk");
-        book.addRecipe(r);
-
+    public void testGetRecipes() {
         assertEquals(book.getRecipes().length, 4);
-   }
+    }
+
+    @Test
+    public void testGetRecipesWrong() {
+        Recipe[] recipes = new Recipe[5];
+        assertNotEquals(book.getRecipes().length, recipes.length);
+    }
 
     @Test
     public void testAddRecipeTrue() {
@@ -43,19 +46,49 @@ public class RecipeBookTest extends TestCase {
     }
 
     @Test
-    public void testAddRecipeFalse() {
-        book.addRecipe(r);
-        book.addRecipe(r);
-        book.addRecipe(r);
-        book.addRecipe(r);
+    public void testAddRecipeNew() {
+        Recipe recipeNew = new Recipe();
+        recipeNew.setName("milk");
+        book.addRecipe(recipeNew);
+        assertTrue(book.addRecipe(r));
+    }
+
+    @Test
+    public void testAddRecipeSame() {
+        r.setName("milk");
+        Recipe milk = new Recipe();
+        milk.setName("milk");
+        book.addRecipe(milk);
+        assertFalse(book.addRecipe(r));
+    }
+
+    @Test
+    public void testAddRecipeFalseNoSpace() {
+        Recipe r1 = new Recipe();
+        r1.setName("sugar");
+        Recipe r2 = new Recipe();
+        r2.setName("milk");
+        Recipe r3 = new Recipe();
+        r3.setName("chocolate");
+        Recipe r4 = new Recipe();
+        r4.setName("coffee");
+        book.addRecipe(r1);
+        book.addRecipe(r2);
+        book.addRecipe(r3);
+        book.addRecipe(r4);
         assertFalse(book.addRecipe(r));
     }
 
     @Test
     public void testDeleteRecipeNull() throws RecipeException {
-        r.setName("milk");
         book.addRecipe(r);
         assertNull(book.deleteRecipe(1));
+    }
+
+    @Test
+    public void testDeleteRecipeEmpty() throws RecipeException {
+        book.addRecipe(r);
+        assertEquals(book.deleteRecipe(0), "");
     }
 
     @Test
