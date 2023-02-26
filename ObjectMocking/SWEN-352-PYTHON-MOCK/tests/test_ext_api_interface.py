56b53c73b81bf6cd19a495741c49e7e74b19a690
ego4995 <ego4995@rit.edu>
2023-02-25 19:57:47 -0500

    what

--- ObjectMocking/SWEN-352-PYTHON-MOCK/tests/test_ext_api_interface.py
+++ ObjectMocking/SWEN-352-PYTHON-MOCK/tests/test_ext_api_interface.py
@@ -12,3 +12,26 @@ class TestExtApiInterface(unittest.TestCase):
             self.books_data = json.loads(f.read())
         with open('tests_data/json_data.txt', 'r') as f:
             self.json_data = json.loads(f.read())
+
+'''    def test_make_request_True(self):
+        attr = {'json.return_value': dict()}
+        requests.get = Mock(return_value = Mock(status_code = 200, **attr))
+        self.assertEqual(self.api.make_request(""), dict())
+
+    def test_make_request_connection_error(self):
+        ext_api_interface.requests.get = Mock(side_effect=requests.ConnectionError)
+        url = "some url"
+        self.assertEqual(self.api.make_request(url), None)
+
+    def test_make_request_False(self):
+        requests.get = Mock(return_value=Mock(status_code=100))
+        self.assertEqual(self.api.make_request(""), None)
+
+    def test_get_ebooks(self):
+        self.api.make_request = Mock(return_value=self.json_data)
+        self.assertEqual(self.api.get_ebooks(self.book), self.books_data)
+
+    def test_get_book_info(self):
+        self.api.make_request = Mock(return_value=self.json_data)
+        print(self.api.get_book_info(self.book))
+        self.assertEqual(self.api.get_book_info(self.book), self.json_data)'''
